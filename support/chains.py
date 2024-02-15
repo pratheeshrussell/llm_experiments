c175b1b5c23ba2fcfb28a30be73b1efad3801c51
pratheeshrussell <pratheeshrussell@gmail.com>
2024-02-16 01:00:14 +0530

    experimenting with device type

--- support/chains.py
+++ support/chains.py
@@ -50,26 +50,36 @@ def generate_image(description: str) -> str:
     # Atleast an empty "negative_prompt" is needed
     # Negative prompt taken from seaart ai
     negative_prompt="verybadimagenegative_v1.3, ng_deepnegative_v1_75t, (ugly face:0.8),cross-eyed,sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, bad anatomy, DeepNegative, facing away, tilted head, {Multiple people}, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worstquality, low quality, normal quality, jpegartifacts, signature, watermark, username, blurry, bad feet, cropped, poorly drawn hands, poorly drawn face, mutation, deformed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, extra fingers, fewer digits, extra limbs, extra arms,extra legs, malformed limbs, fused fingers, too many fingers, long neck, cross-eyed,mutated hands, polar lowres, bad body, bad proportions, gross proportions, text, error, missing fingers, missing arms, missing legs, extra digit, extra arms, extra leg, extra foot, ((repeating hair))"
-    img_width=768
-    img_height=768
-    inference_steps=50
-    guidance_scale=7
+    img_width=512
+    img_height=512
+    inference_steps=25
+    guidance_scale=9
     seed=random.randint(100, 1000000)
-
+    device="cpu"
     # Using repo path
     repo_path="digiplay/Juggernaut_final"
     pipe = StableDiffusionPipeline.from_pretrained(
         repo_path,
-        torch_dtype=torch.float32,
+        variant="fp16", 
+        torch_dtype=torch.float16, 
+        low_cpu_mem_usage=True,
         use_safetensors=True,)
     pipe.safety_checker = None
     pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
+    pipe=pipe.to(device)
     
+    # need to experiment with these options
+    if (device=='cuda'):
+        # pipe.enable_xformers_memory_efficient_attention()
+        pipe.enable_vae_slicing()
+        #pipe.enable_sequential_cpu_offload()
+        pipe.enable_attention_slicing()
     # Tokenizer long prompt issues
     compel = Compel(
         tokenizer=pipe.tokenizer, 
         text_encoder=pipe.text_encoder,
-        truncate_long_prompts=False
+        truncate_long_prompts=False,
+        device=device
     )
     conditioning = compel.build_conditioning_tensor(description)
     negative_conditioning = compel.build_conditioning_tensor(negative_prompt)
@@ -79,7 +89,7 @@ def generate_image(description: str) -> str:
     #print(negative_prompt_embeds.shape)
     # generate args
     # Atleast an empty "negative_prompt_embeds" is needed
-    generator = torch.Generator("cpu").manual_seed(seed)
+    generator = torch.Generator(device).manual_seed(seed)
     new_args = {
         "prompt_embeds":prompt_embeds,
         "negative_prompt_embeds": negative_prompt_embeds,
@@ -90,8 +100,7 @@ def generate_image(description: str) -> str:
         "num_images_per_prompt":1,
         "generator":generator
     }
-
-    pipe.enable_sequential_cpu_offload()
+    
     images = pipe(**new_args).images
     image=images[0]
     image_path = "image/" + datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".jpg"
